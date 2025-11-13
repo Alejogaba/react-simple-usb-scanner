92efab4bdf36dc15d720508e3e2ae50a1a40d383
Diego Garces <alejogaba@gmail.com>
2025-11-13 13:18:54 -0500

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts' // ¡Importa esto!
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ insertTypesEntry: true }) // ¡Añade esto!
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ReactSimpleUsbScanner',
+      // Asegúrate de que esto coincida con tu package.json
+      fileName: (format) => `react-simple-usb-scanner.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'], // ¡Externaliza React!
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
